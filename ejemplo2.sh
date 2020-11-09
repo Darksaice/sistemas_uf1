ef2767cec5b88bd964d32cbe820629338b6645a7
Darksaice <mitjansperezsara@gmail.com>
2020-11-09 12:05:27 +0100

    Ejemplo de variables y pacticando vim

--- ejemplo2.sh
+++ ejemplo2.sh
@@ -1,4 +1,22 @@
 ls -la | head -n 2
 date
+
 cal
-ola ke ase
+adsdadrrfsdd
+cero
+uno
+dos
+tres
+cuatro
+cinco
+seis
+siete
+ocho
+jkshdfjksdfhjks klashjdfkhsfsdf
+
+kjsdhffffka
+echo ola buenos la mañana ya os vale despertad
+
+cowsay ola k ase
+
+ping -c 3 google.es
---
+++ saludo.sh
@@ -0,0 +1,7 @@
+NOMBRE_VACA="Pepa"
+
+echo "Hola! ¿Quién eres?"
+
+read NOMBRE
+
+echo "Hola $NOMBRE, soy $NOMBRE_VACA.  ¿Qué tal?"
